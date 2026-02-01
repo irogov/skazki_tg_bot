77b602dc7e228b3a5fd14274da7f86f80ffd2335
Ilya Rogov <irogoff88@gmail.com>
2026-02-01 12:40:29 +1000

    normalized text

--- services/payment_service.py
+++ services/payment_service.py
@@ -17,6 +17,7 @@ async def buy(message: types.Message):
     currency=CURRENCY,
     prices=[PRICE],
     need_email=True,
+    send_email_to_provider = True,
     start_parameter='one-month-subscription',
     payload=f'sub_30days_{message.from_user.id}'  # ✅ Уникальный payload
 )
